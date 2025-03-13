9e75a8693fd094e6ae35ab62c7e60950765dfa7a
ST10441678 <ST10441678@vcconnect.edu.za>
2025-03-13 15:21:44 +0200

    Add project files.

---
+++ ST10441678_IceTask3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ST10441678_IceTask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
